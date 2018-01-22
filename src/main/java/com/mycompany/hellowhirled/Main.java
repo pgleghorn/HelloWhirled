a83633a7d639c25152ce4ea91f0cfb5968b268d8
Phil Gleghorn <pgleghorn@gmail.com>
2018-01-22 12:53:27 +0000

    Update Main.java

--- src/main/java/com/mycompany/hellowhirled/Main.java
+++ src/main/java/com/mycompany/hellowhirled/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
     public static void main(String args[]) {
         System.out.println("Hello whirled Lorem ipsum dolor sit amet, consectetur adipiscing elit!!!!");
+        System.out.println("pull request test");
     }
     
 }
